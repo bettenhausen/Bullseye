0209ccfbe606f4066740299f8240a6ff7faaca4b
Ryan Bettenhausen <50972610+bettenhausen@users.noreply.github.com>
2022-03-12 16:28:33 -0600

    moved constants new file

--- Bullseye/Views/RoundViews.swift
+++ Bullseye/Views/RoundViews.swift
@@ -14,10 +14,10 @@ struct RoundedImageViewStroked: View {
         Image(systemName: systemName)
             .font(.title)
             .foregroundColor(Color("TextColor"))
-            .frame(width: 56.0, height: 56.0)
+            .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
             .overlay(
                 Circle()
-                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0))
+                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: Constants.General.strokeWidth))
     }
 }
 
@@ -28,7 +28,7 @@ struct RoundedImageViewFilled: View {
         Image(systemName: systemName)
             .font(.title)
             .foregroundColor(Color("ButtonFilledTextColor"))
-            .frame(width: 56.0, height: 56.0)
+            .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
             .background(Circle()
                             .fill(Color("ButtonFilledBackgroundColor")))
     }
@@ -43,10 +43,10 @@ struct RoundRectTextView: View {
             .font(.title3)
             .foregroundColor(Color("TextColor"))
             .kerning(-0.2)
-            .frame(width: 68.0, height: 56.0)
+            .frame(width: Constants.General.roundRectViewWidth, height: Constants.General.roundRectViewHeight)
             .overlay(
                 RoundedRectangle(cornerRadius: 21)
-                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0))
+                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: Constants.General.strokeWidth))
     }
 }
 
